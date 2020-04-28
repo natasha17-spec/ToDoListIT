4e220389630507a5791e7d911133b8e4d3611426
natasha17 <17061990@inbox.ru>
2020-04-28 13:08:30 +0300

    Updates

---
+++ precache-manifest.acba626a9e1f1c4967b7bdf4e4fc6209.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39dfa4248f78831f1ac96240257f73ee",
+    "url": "/ToDoListIT/index.html"
+  },
+  {
+    "revision": "b0c13a2c092b4e75e087",
+    "url": "/ToDoListIT/static/css/main.e0746b25.chunk.css"
+  },
+  {
+    "revision": "5ae5a40e0d4525d3cd62",
+    "url": "/ToDoListIT/static/js/2.7db00c3d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ToDoListIT/static/js/2.7db00c3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0c13a2c092b4e75e087",
+    "url": "/ToDoListIT/static/js/main.20379675.chunk.js"
+  },
+  {
+    "revision": "21254de34198e65c4a4b",
+    "url": "/ToDoListIT/static/js/runtime-main.d8b64730.js"
+  },
+  {
+    "revision": "28ba06a81506c9b81c79102701f89894",
+    "url": "/ToDoListIT/static/media/Bean Eater-1s-200px.28ba06a8.svg"
+  },
+  {
+    "revision": "5dd1c945782e35ee7fc5acb6f5856248",
+    "url": "/ToDoListIT/static/media/shvetsiia.5dd1c945.jpg"
+  }
+]);
